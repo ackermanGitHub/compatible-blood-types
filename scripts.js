1e43755bf2385941ade2fa2d62273082772cb8df
ackermanGitHub <julio.sergio2709@gmail.com>
2022-08-14 04:18:53 -0400

    still working on the CSS

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ const closeBtn = document.querySelector('.close-btn');
 const toggleBtn = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 
+const inputSelectorsId = ['A+_selector', 'A-_selector', 'B+_selector', 'B-_selector', 'AB+_selector', 'AB-_selector', 'O+_selector', 'O-_selector'];
+inputSelectorsId.forEach(id => {
+    document.getElementById(id+'-id').addEventListener('click', () => {
+        console.log(id);
+    })
+})
+
 toggleBtn.addEventListener('click', function(){
     sidebar.classList.toggle('show-sidebar');
 });
